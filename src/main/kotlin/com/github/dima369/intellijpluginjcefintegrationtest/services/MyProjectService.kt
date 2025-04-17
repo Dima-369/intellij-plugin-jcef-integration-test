488016a3bf14b632b4f15a4a91ea5352a4b89e86
GitHub Action <action@github.com>
2025-04-17 18:41:20 +0000

    Template cleanup

--- src/main/kotlin/com/github/dima369/intellijpluginjcefintegrationtest/services/MyProjectService.kt
+++ src/main/kotlin/com/github/dima369/intellijpluginjcefintegrationtest/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.dima369.intellijpluginjcefintegrationtest.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.dima369.intellijpluginjcefintegrationtest.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
--- src/main/kotlin/com/github/dima369/intellijpluginjcefintegrationtest/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/dima369/intellijpluginjcefintegrationtest/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.dima369.intellijpluginjcefintegrationtest.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
